82638925a004492b368c1d74b0ffd372befeb100
Joshua Kummer <kummer.joshua@outlook.com>
2022-08-18 23:57:46 +0200

    Add "Last updated on .."

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/0ptim/DeFiChain-Wiki/tree/main/",
+          showLastUpdateTime: true,
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
