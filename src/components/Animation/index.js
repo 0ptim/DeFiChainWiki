416e3e7315b14cf7c85c3c55cac4362cfa3b40bf
Joshua Kummer <kummer.joshua@outlook.com>
2022-08-26 22:51:24 +0200

    Add first animation to home page

---
+++ src/components/Animation/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useRef } from "react";
+import styles from "./styles.module.css";
+
+import * as THREE from "three";
+import BIRDS from "vanta/dist/vanta.birds.min";
+
+export const BackgroundAnimation = () => {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: vantaRef.current,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color1: "#FC1DAE",
+          color2: "#00F6FF",
+          colorMode: "lerpGradient",
+          birdSize: 0.9,
+          quantity: 4,
+          separation: 60,
+          backgroundAlpha: 0
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+  return (
+    <div className={styles.animationOverlay} ref={vantaRef}>
+    </div>
+  );
+};
