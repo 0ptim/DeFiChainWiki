fca33fee37e8aa838cf3abdf5b6b75f2b480473e
Joshua Kummer <kummer.joshua@outlook.com>
2023-02-04 23:55:13 +0100

    Add base functionality to window

--- src/components/QA/index.js
+++ src/components/QA/index.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
 
 export default function QA() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.window}>
-                <h1>State: Ask a question</h1>
-                <input className={styles.input} type="text" placeholder="How many DFI do I need to run my own masternode?" />
-                <button className={styles.button}>Ask</button>
-                <p className={styles.answer}>ANSWER</p>
-            </div>
-        </div>
-    );
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://chatdefichain.fly.dev/ask", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ question }),
+      });
+      const data = await response.json();
+      setAnswer(data.response.response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.window}>
+        <h1>State: {loading ? "Generating answer" : "Ask a question"}</h1>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="How many DFI do I need to run my own masternode?"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <button
+          className={styles.button}
+          onClick={handleSubmit}
+          disabled={loading}
+        >
+          Ask
+        </button>
+        <p className={styles.answer}>{answer}</p>
+      </div>
+    </div>
+  );
 }
