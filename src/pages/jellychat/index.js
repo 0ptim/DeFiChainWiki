69cebab4df889f39934e788e95756f7e83659cd0
Joshua Kummer <kummer.joshua@outlook.com>
2023-02-09 21:32:42 +0100

    Implement JellyChat with tailwind

--- src/pages/jellychat/index.js
+++ src/pages/jellychat/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import styles from "./styles.module.css";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 
@@ -39,14 +38,16 @@ function QA() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.window}>
+    <div className="transition-color mapBackground py-28 flex justify-center items-center bg-center bg-cover bg-no-repeat">
+      <div className="relative flex flex-col items-center shadow-2xl max-w-2xl grow rounded-3xl p-10 bg-white dark:bg-backgroundDark">
         <h1>JellyChat</h1>
         <p>Ask any question about DeFiChain</p>
         <input
-          className={`${styles.input} ${error ? styles.error : ""}`}
+          className={`w-full outline-none text-xl p-5 rounded-lg shadow-md border border-transparent bg-elementLight hover:border-main-300 focus:border-main-700 dark:bg-elementDark  dark:hover:border-main-700 dark:focus:border-main-500 ${
+            error ? "bg-red-100 dark:bg-main-900" : ""
+          }`}
           type="text"
-          placeholder="How many DFI do I need to run my own masternode?"
+          placeholder="What is DeFiChain?"
           value={question}
           onChange={(e) => {
             setQuestion(e.target.value);
@@ -55,23 +56,26 @@ function QA() {
           onKeyDown={handleKeyDown}
         />
         <button
-          className={styles.sendButton}
+          className="sendButton w-full outline-none border-none text-lg p-4 rounded-lg text-white cursor-pointer my-3"
           onClick={handleSubmit}
           disabled={loading}
         >
           Send
         </button>
-        <div className={styles.answer}>
-          <p>{answer}</p>
+        <div className="w-full border-0 outline-none text-base p-5 h-48 rounded-lg shadow-md overflow-auto bg-elementLight dark:bg-elementDark">
+          {!loading && <p>{answer}</p>}
           {loading && (
             <div>
-              <div className={`skeleton skeleton1`}></div>
-              <div className={`skeleton skeleton2`}></div>
-              <div className={`skeleton skeleton3`}></div>
+              <div className="w-80 h-4 animate-pulse mb-2 rounded-md bg-gray-700"></div>
+              <div className="w-40 h-4 animate-pulse mb-2 rounded-md bg-gray-700"></div>
+              <div className="w-48 h-4 animate-pulse mb-2 rounded-md bg-gray-700"></div>
             </div>
           )}
         </div>
-        <Link className={styles.helpButton} to="/docs/auto/JellyChat">
+        <Link
+          className="absolute top-5 right-5 shadow-md rounded-full w-10 h-10 flex items-center justify-center cursor-pointer font-bold bg-elementLight dark:bg-elementDark"
+          to="/docs/auto/JellyChat"
+        >
           ?
         </Link>
       </div>
