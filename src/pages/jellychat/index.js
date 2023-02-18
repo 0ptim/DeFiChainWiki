631a9b21e575a71a6beb8c460cba585a2df74199
Joshua Kummer <kummer.joshua@outlook.com>
2023-02-18 23:58:23 +0100

    Update backend url

--- src/pages/jellychat/index.js
+++ src/pages/jellychat/index.js
@@ -27,7 +27,7 @@ export default function JellyChat() {
     setCurrentId(0);
 
     try {
-      const response = await fetch("https://chatdefichain.fly.dev/ask", {
+      const response = await fetch("https://jellychat.fly.dev/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question }),
@@ -131,7 +131,7 @@ function Rating({ selectedRating, currentId, setSelectedRating }) {
     if (currentId === 0) return;
 
     try {
-      const response = await fetch("https://chatdefichain.fly.dev/rate", {
+      const response = await fetch("https://jellychat.fly.dev/rate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ id: currentId, rating: rating }),
