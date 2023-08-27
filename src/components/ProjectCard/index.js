a782723ded3f2541004883f942fc029247a63b4a
0ptim <kummer.joshua@outlook.com>
2023-08-27 19:31:57 +0200

    Light mode adjustments

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,11 +3,13 @@ import Tag from "../Tag";
 
 export default function ProjectCard({ project }) {
   return (
-    <div className="w-80 cursor-pointer overflow-hidden rounded-md bg-slate-800 transition-transform ease-out hover:scale-105 hover:shadow-lg">
+    <div className="w-80 cursor-pointer overflow-hidden rounded-md bg-slate-200 transition-all ease-out hover:scale-105 hover:shadow-md dark:bg-slate-800">
       <img src={project.img} className="" />
       <div className="flex flex-col p-6">
         <h2 className="mb-1 text-xl">{project.name}</h2>
-        <p className="text-md m-0 text-slate-400">{project.description}</p>
+        <p className="text-md m-0 text-slate-600 dark:text-slate-400">
+          {project.description}
+        </p>
         <div className="mt-4 flex flex-wrap gap-2">
           {project.tags.map((tag) => (
             <Tag tag={tag} active={true} />
