efa447317575236fcfc9a5d118b5140d3621d088
0ptim <kummer.joshua@outlook.com>
2023-08-27 20:35:08 +0200

    Improve tag filtering logic

--- src/utils/filterProjects.js
+++ src/utils/filterProjects.js
@@ -3,7 +3,7 @@ export const filterProjects = (projects, activeTags, inputValue) => {
 
   return projects.filter(
     (project) =>
-      project.tags.some((tag) => activeTags.includes(tag)) &&
+      activeTags.every((tag) => project.tags.includes(tag)) &&
       (project.name.toLowerCase().includes(lowerCaseInputValue) ||
         project.description.toLowerCase().includes(lowerCaseInputValue) ||
         project.x.toLowerCase().includes(lowerCaseInputValue) ||
