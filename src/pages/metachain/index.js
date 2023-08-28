30ecda5824e37686971a0093d5fb84e29d042134
0ptim <kummer.joshua@outlook.com>
2023-08-28 19:55:37 +0200

    Add background image layout

--- src/pages/metachain/index.js
+++ src/pages/metachain/index.js
@@ -19,26 +19,26 @@ export default function Metachain() {
 
   return (
     <Layout description="Find projects on DeFi Meta Chain">
-      <div className="px-4 md:px-8">
-        <div className="flex flex-col items-center justify-center gap-8 px-2 py-20 md:px-40 2xl:px-96">
-          <h1>
-            <Translate>Metachain.Title</Translate>
-          </h1>
-          <Input onSubmit={onSubmit} />
-          <div className="flex flex-wrap justify-center gap-2 px-4 md:px-8">
-            {tags &&
-              tags.map((tag) => (
-                <Tag
-                  key={tag.name}
-                  tag={tag.name}
-                  allTags={tags}
-                  active={activeTags.includes(tag.name)}
-                  onClick={() => handleTagClick(tag.name)}
-                />
-              ))}
-          </div>
+      <div className="dmc-bg flex flex-col items-center justify-center gap-8 px-2 py-20 md:px-40 2xl:px-96">
+        <h1>
+          <Translate>Metachain.Title</Translate>
+        </h1>
+        <Input onSubmit={onSubmit} />
+        <div className="flex flex-wrap justify-center gap-2 px-4 md:px-8">
+          {tags &&
+            tags.map((tag) => (
+              <Tag
+                key={tag.name}
+                tag={tag.name}
+                allTags={tags}
+                active={activeTags.includes(tag.name)}
+                onClick={() => handleTagClick(tag.name)}
+              />
+            ))}
         </div>
+      </div>
 
+      <div className="px-4 pt-8 md:px-8">
         {searchInput || activeTags.length !== tags.length ? (
           <div>
             <h2>Search results</h2>
