3cf21d117ef7159db8cc2f41e33e3a3764543700
0ptim <kummer.joshua@outlook.com>
2023-08-31 20:35:59 +0200

    Improve input UI

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -8,7 +8,7 @@ export default function Input({ onSubmit }) {
 
   return (
     <input
-      className={`w-full rounded-lg border border-transparent bg-gray-50 p-5 pr-11 text-xl shadow-md outline-none hover:border-main-300 focus:border-main-700 dark:bg-gray-800  dark:hover:border-main-700 dark:focus:border-main-500`}
+      className={`w-full rounded-3xl border-2 border-transparent bg-gray-50 p-5 pr-11 text-xl shadow-md outline-none backdrop-blur-sm transition-colors placeholder:text-gray-200 hover:border-main-300 focus:border-main-700 dark:border-main-700 dark:bg-black/40 dark:hover:border-main-600 dark:focus:border-main-500`}
       type="text"
       placeholder={translate({ message: "Metachain.Placeholder" })}
       onChange={(e) => {
