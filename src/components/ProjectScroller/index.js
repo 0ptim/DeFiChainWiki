36693b1d1b44cd31fdb92438a8b9c43ca2e3ee6f
0ptim <kummer.joshua@outlook.com>
2023-09-04 18:16:17 +0200

    Hide arrow buttons when at end

--- src/components/ProjectScroller/index.js
+++ src/components/ProjectScroller/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ProjectCard from "../ProjectCard";
 import {
   faChevronRight,
@@ -8,6 +8,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ProjectScroller({ title, projects, tags }) {
   const scrollerRef = useRef(null);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(false);
+
+  useEffect(() => {
+    const { scrollWidth, clientWidth } = scrollerRef.current;
+    setCanScrollRight(scrollWidth > clientWidth);
+
+    const handleScroll = () => {
+      setCanScrollLeft(scrollerRef.current.scrollLeft > 0);
+      setCanScrollRight(
+        scrollerRef.current.scrollLeft < scrollWidth - clientWidth
+      );
+    };
+
+    scrollerRef.current.addEventListener("scroll", handleScroll);
+
+    return () => {
+      if (scrollerRef.current) {
+        scrollerRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
 
   const scroll = (scrollOffset) => {
     scrollerRef.current.scrollLeft += scrollOffset;
@@ -15,23 +37,27 @@ export default function ProjectScroller({ title, projects, tags }) {
 
   return (
     <div className="relative">
-      <div className="absolute bottom-0 left-0 top-0 z-10 flex items-center">
-        <button
-          className="h-10 w-10 cursor-pointer rounded-full border-none bg-gray-700 transition-all ease-out hover:scale-110"
-          onClick={() => scroll(-200)}
-        >
-          <FontAwesomeIcon icon={faChevronLeft} />
-        </button>
-      </div>
+      {canScrollLeft && (
+        <div className="absolute bottom-0 left-0 top-0 z-10 flex items-center">
+          <button
+            className="h-10 w-10 cursor-pointer rounded-full border-none bg-gray-700 transition-all ease-out hover:scale-110"
+            onClick={() => scroll(-200)}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+        </div>
+      )}
 
-      <div className="absolute bottom-0 right-0 top-0 z-10 flex items-center">
-        <button
-          className="h-10 w-10 cursor-pointer rounded-full border-none bg-gray-600 transition-all ease-out hover:scale-110"
-          onClick={() => scroll(200)}
-        >
-          <FontAwesomeIcon icon={faChevronRight} />
-        </button>
-      </div>
+      {canScrollRight && (
+        <div className="absolute bottom-0 right-0 top-0 z-10 flex items-center">
+          <button
+            className="h-10 w-10 cursor-pointer rounded-full border-none bg-gray-600 transition-all ease-out hover:scale-110"
+            onClick={() => scroll(200)}
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+      )}
 
       <h2>{title}</h2>
       <div
