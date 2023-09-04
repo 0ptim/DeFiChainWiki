f6b72f7a86a5b87e447ef143260023fe2f1ad417
0ptim <kummer.joshua@outlook.com>
2023-09-05 00:16:04 +0200

    Improve social button

--- src/components/SocialButton/index.js
+++ src/components/SocialButton/index.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Button = ({ href, label, icon }) => (
   <a href={href} target="_blank" rel="noreferrer noopener" aria-label={label}>
-    <div className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white text-black shadow-sm transition-all ease-out hover:scale-110">
+    <div className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-black text-lg text-violet-200 opacity-70 shadow-sm transition-all ease-out hover:scale-110 hover:opacity-90">
       <FontAwesomeIcon icon={icon} />
     </div>
   </a>
