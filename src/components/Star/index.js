f3dc61657b46db92974ef8f7ad249f66a6c38ff1
0ptim <kummer.joshua@outlook.com>
2023-09-04 18:31:21 +0200

    Fix stars at full opacity on page laod

--- src/components/Star/index.js
+++ src/components/Star/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Star = ({ style }) => (
   <div
     className="absolute h-0.5 w-0.5 animate-blink bg-white"
-    style={style}
+    style={{ ...style, opacity: 0 }}
   ></div>
 );
 
