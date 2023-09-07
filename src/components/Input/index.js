9bfa5886362706c318751517ac0ccba96c75e7dc
0ptim <kummer.joshua@outlook.com>
2023-09-07 22:56:40 +0200

    Remove dark mode because dark is default

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,7 +7,7 @@ export default function Input({ onSubmit }) {
 
   return (
     <input
-      className={`w-full rounded-3xl border-2 border-transparent bg-gray-50 p-5 pr-11 text-xl shadow-md outline-none backdrop-blur-sm transition-colors placeholder:text-gray-200 hover:border-main-300 focus:border-main-700 dark:border-main-700 dark:bg-black/40 dark:hover:border-main-600 dark:focus:border-main-500`}
+      className={`w-full rounded-3xl border-2 border-main-700 bg-black/40 p-5 pr-11 text-xl shadow-md outline-none backdrop-blur-sm transition-colors placeholder:text-gray-200 hover:border-main-600 focus:border-main-500`}
       type="text"
       placeholder="Search for a project..."
       onChange={(e) => {
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -12,14 +12,12 @@ export default function ProjectCard({ project, allTags }) {
   return (
     <div
       onClick={handleClick}
-      className="w-80 cursor-pointer overflow-hidden rounded-md bg-slate-200 shadow-md transition-all ease-out hover:scale-105 hover:shadow-lg dark:bg-slate-800"
+      className="w-80 cursor-pointer overflow-hidden rounded-md bg-slate-800 shadow-md transition-all ease-out hover:scale-105 hover:shadow-lg"
     >
       <img src={project.img} className="" />
       <div className="flex flex-col p-6">
         <h2 className="mb-1 text-xl">{project.name}</h2>
-        <p className="text-md m-0 text-slate-600 dark:text-slate-400">
-          {project.description}
-        </p>
+        <p className="text-md m-0 text-slate-400">{project.description}</p>
         <div className="mt-4 flex flex-wrap gap-2">
           {project.tags.map((tag) => (
             <Tag key={tag} tag={tag} allTags={allTags} active={true} />
